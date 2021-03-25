6a0e6bc8f7ea92e74c6e05531106157606c72b49
JAVA1 <81381886+pd0x000@users.noreply.github.com>
2021-03-25 21:59:48 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+    static void fisrtExercise() {
+        byte a = 3 ;
+        byte b = 5 ;
+        byte c = 4 ;
+        System.out.println(a + " " + b + " " + c);
+        System.out.println("_______________________");
+    }
+    static void secondExercise() {
+        int a = 512343 ;
+        int b = 3431231 ;
+        System.out.println(a + b);
+        System.out.println("_______________________");
+    }
+    static void thirdExercise() {
+        int variabile = 123456 ;
+            System.out.println(variabile);
+        System.out.println("_______________________");
+    }
+    public static void main(String[] args) {
+        System.out.println("First exercise : \n");
+        fisrtExercise();
+        System.out.println("Second exercise : \n");
+        secondExercise();
+        System.out.println("Third exercise : \n");
+        thirdExercise();
+    }
+}
